5273a6c925c7a5c1c1b55e6e6e635ac4fa1c5545
dploure <riku.991@gmail.com>
2024-05-21 22:10:12 +0200

    primer commit

---
+++ src/main/java/com/mycompany/pruebatoken/Pruebatoken.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pruebatoken;
+
+/**
+ *
+ * @author david
+ */
+public class Pruebatoken {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
